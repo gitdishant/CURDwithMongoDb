c1b777e729aa8c49179dda1f31a311e0625fadbf
dishant <randomv637@gmail.com>
2025-03-21 23:52:33 +0530

    Post method on mongoDb

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.models.js")
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home");
+});
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product)
+    
+  } catch (error) {
+    res.status(500).json({massage : error.massage})
+  }
+})
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -12,11 +30,7 @@ mongoose
       console.log(`welcome to port 3001`);
     });
   })
-
   .catch(() => {
     console.error("not running");
   });
 
-app.get("/", (req, res) => {
-  res.send("Welcome Home");
-});
