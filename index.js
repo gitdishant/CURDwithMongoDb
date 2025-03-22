0625f05ff0f0fbccc35d74f272a8dbc8253cf752
dishant <randomv637@gmail.com>
2025-03-23 00:25:30 +0530

    geting Api form mongoDB and Using id to get specific product

--- index.js
+++ index.js
@@ -18,9 +18,54 @@ app.post('/api/products', async (req, res) => {
     res.status(200).json(product)
     
   } catch (error) {
-    res.status(500).json({massage : error.massage})
+    res.status(500).json({message : error.message})
   }
 })
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const product = await Product.find({})
+        res.status(200).json(product)
+    } catch (error) {
+      res.status(500).json({message : error.message})
+    }
+});
+
+/*app.get('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+ if (!mongoose.Types.ObjectId.isValid(id)) 
+
+{                        
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+
+ //it initialsize the findById methos
+
+    const product = await Product.findById(id);
+
+   if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+   }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});*/
+
+// if this not work try the code above 
+app.get('/api/products/:id', async (req ,res) => {
+   try {
+     const { id } = req.params;
+      const product = await Product.findById(id); 
+      res.status(200).json(product) 
+    } catch (error) { 
+      res.status(500).json({message : error.message}) 
+    }
+   });
+
 
 mongoose
   .connect(process.env.MONGODB_URL)
