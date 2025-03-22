0d56888b0f33e25320cf9fc961dfe1349908b760
dishant <randomv637@gmail.com>
2025-03-22 10:35:58 +0530

    push command

--- models/product.models.js
+++ models/product.models.js
@@ -1,2 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const productDetails = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default : 0
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+ const Product = mongoose.model("Product", productDetails);
+
+module.exports = Product;
